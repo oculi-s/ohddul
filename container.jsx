d99f9cb13a114e56a941abf47b376f50b153cc2c
oculi-s <oculis0925@yonsei.ac.kr>
2023-07-03 20:49:13 +0900

    20230703_204900

--- container.jsx
+++ container.jsx
@@ -84,6 +84,8 @@ const HEAD = () => {
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no"></meta>
 			<link rel="icon" href="/favicon.ico" />
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@latest/css/font-awesome.min.css"></link>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1762696463079495"
+				crossorigin="anonymous"></script>
 		</Head>
 	)
 }
