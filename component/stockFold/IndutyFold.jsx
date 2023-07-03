811f1fbe2498bd485c96e9275c02bac698b60a2d
oculi-s <oculis0925@yonsei.ac.kr>
2023-07-03 18:18:46 +0900

    20230703_181800

--- component/stockFold/IndutyFold.jsx
+++ component/stockFold/IndutyFold.jsx
@@ -4,95 +4,30 @@ import Fold from '#/base/Fold';
 import json from '@/module/json';
 import '@/module/array'
 
-const setStock = ({ induty, code, meta, price, dict, stockDict }) => {
-    const stock = Object.entries(induty)
-        .filter(([e, v]) => v == code)
-        .filter(([e, v]) =>
-            meta[e]?.a && price[e]?.c)
-        .sort(([a, _1], [b, _2]) =>
-            meta[b]?.a * price[b]?.c -
-            meta[a]?.a * price[a]?.c
-        )
-    stock.forEach(([e, v]) => {
-        const name = meta[e]?.n;
-        const amount = meta[e]?.a;
-        const close = price[e]?.c;
-        const stockKey = `<a href=/stock/${e}>${name}</a>`
-        dict[stockKey] = `<td>${Price(amount * close) || "-"}</td>`;
-        stockDict[e] = 1;
-    });
-}
 
 /**
- * 재귀함수인데 너무 오래걸림
+ * json to table과 재귀함수 이용해서 만들었다가
+ * 데이터 구조 바꾸고, 재귀함수 시간 문제 때문에 폐기하고
  * 
- * induty.json에 count를 미리 저장해둘것 2023.06.30 추가완료
+ * 2023.07.03 원점에서 다시 제작
  */
-const setIndex = ({
-    data, dict, key, origin, index, induty,
-    i, meta, price, stockDict, go = true
-}) => {
-    if (!data) return;
-    if (!data._) return;
-    const cnt = data?._?.c;
-    if (!cnt) return;
-    const next = i ? key.slice(1) : key.slice(2);
-    key = i ? key.slice(0, 1) : key.slice(0, 2);
-    const name = index?.index[data?._?.n];
-    const code = data?._?.n?.slice(1);
-    const newKey = code ? `<a href=/induty/${code}>${name}</a> (${cnt}개)` : name;
-    dict[newKey] = { isth: true, cs: 2 };
-    const validKeys = Object.keys(data).filter(e => e != '_');
-    const nextProps = {
-        i: i + 1, meta, price, origin,
-        key: next, index, induty,
-        data: data[key],
-        dict: dict[newKey],
-        stockDict, code
-    }
-    if (key.length) { // key가 남아있으면 그 key에 대해서만 진행
-        if (next.length) {
-            delete dict[newKey];
-            nextProps.dict = dict;
-        }
-        setIndex(nextProps)
-    } else if (validKeys.length && go) { // code를 prefix로 하는 하위 induty
-        validKeys.forEach(key => {
-            nextProps.data = data[key];
-            nextProps.go = false;
-            setIndex(nextProps);
-        })
-    }
-    if (code == origin) {
-        setStock(nextProps);
-    }
-}
-
 const IndutyFold = ({
     meta, code, price, induty, index, router,
     folded = true
 }) => {
     induty = induty?.data;
+    index = index?.data;
     meta = meta?.data;
     if (!induty) return <></>;
     const stockInduty = Big(induty[code] || code);
-    const iname = index?.index[stockInduty];
-    if (!iname) return <></>;
-    const data = index?.data[stockInduty[0]];
+    const data = index[stockInduty];
     if (!data) return <></>;
     const dict = {};
-    const stockDict = {};
-    const key = stockInduty.slice(1);
-    setIndex({
-        data, dict, key,
-        origin: key,
-        i: 0, index, induty, meta, price, stockDict
-    });
-    const stock = Object.keys(stockDict).filter(e => meta[e] && price[e]);
-    const total = stock.map(e => meta[e].a * price[e].c || 0).sum();
+    const stock = Object.keys(induty).filter(e => induty[e] == induty[code]);
+    const total = stock.map(e => meta[e]?.a * price[e]?.c || 0).sum();
     const name = <>
-        <h3 style={{ margin: "10px auto" }}><Link href={`/induty/${key}`}>{iname}</Link></h3>
-        <p>{stock.length} 종목 시총 : ({Price(total)})</p>
+        <h3 style={{ margin: "10px auto" }}><Link href={`/induty/${code}`}>{data?.n}</Link></h3>
+        <p>{data?.c} 종목 시총 : ({Price(total)})</p>
     </>
     const body = json.toTable(dict);
     const props = { router, name, body, folded };
