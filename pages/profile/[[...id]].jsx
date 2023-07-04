5b3060669849c56a9aab09b345d82a5a053bb8be
oculi-s <oculis0925@yonsei.ac.kr>
2023-07-04 21:50:55 +0900

    20230704_215000

--- pages/profile/[[...id]].jsx
+++ pages/profile/[[...id]].jsx
@@ -194,7 +194,6 @@ const Index = ({
             <>{id} : 존재하지 않는 사용자입니다.</>
         );
     }
-    meta = meta.data;
     const score = userMeta[uid]?.rank;
     const [rank, nextRank] = getRank(score);
     const prevScore = Math.floor(score / 100) * 100;
